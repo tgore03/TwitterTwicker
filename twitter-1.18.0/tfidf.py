8ded5d09bfc1c1ed65aeea6fcfd3011499fe3e7b
Shruti Sahu <Shruti@Shrutis-MBP.Home>
2017-11-27 20:27:30 -0600

    tf done

--- twitter-1.18.0/tfidf.py
+++ twitter-1.18.0/tfidf.py
@@ -1,17 +1,31 @@
 import json
-from pprint import pprint
+
 
 def get_tweets():
 	data = json.load(open('data.json'))
-	words_set = set()
+	#words_set = set()
 	tweets = data['tweetno']
+	total = 0
+	arr_tf = []
 	for item in tweets:
 		sentence = item.get("text").split()
-		#print sentence
+		term_frequency = {}
+		tweet_length = 0
 		for word in sentence:
 			if not word.startswith('http'):
-				words_set.add(word)
+				tweet_length+=1.0
+				if word in term_frequency:
+					term_frequency[word] += 1.0
+				else:
+					term_frequency[word] = 1
+		for k,v in term_frequency.items():
+			#print k,v
+			v = v/tweet_length
+			#print k,v
+		arr_tf.append(term_frequency)		
+	
+	print len(arr_tf)	
 
-	print words_set
+	
 
 get_tweets()
