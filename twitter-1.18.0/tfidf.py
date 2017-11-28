aa16c8462cc1f95737b87d2ca3e966d9749ee498
Shruti Sahu <Shruti@Shrutis-MacBook-Pro.local>
2017-11-27 22:05:49 -0600

    tfidf complete

--- twitter-1.18.0/tfidf.py
+++ twitter-1.18.0/tfidf.py
@@ -1,9 +1,9 @@
 import json
-
+import math
 
 def get_tweets():
 	data = json.load(open('data.json'))
-	#words_set = set()
+	words_set = {}
 	tweets = data['tweetno']
 	total = 0
 	arr_tf = []
@@ -13,6 +13,10 @@ def get_tweets():
 		tweet_length = 0
 		for word in sentence:
 			if not word.startswith('http'):
+				if word in words_set:
+					words_set[word] += 1
+				else:
+					words_set[word] = 1
 				tweet_length+=1.0
 				if word in term_frequency:
 					term_frequency[word] += 1.0
@@ -22,10 +26,31 @@ def get_tweets():
 			#print k,v
 			v = v/tweet_length
 			#print k,v
-		arr_tf.append(term_frequency)		
-	
-	print len(arr_tf)	
+		arr_tf.append(term_frequency)	
 
-	
+	total_tweets = len(arr_tf)
+	#print len(words_set) # 4881
+	#print len(arr_tf)	 # 1000
+
+	idf = {}
+	for word in words_set:
+		doc_word_freq = 0
+		for item in arr_tf:
+			if word in item:
+				doc_word_freq += 1
+		#print doc_word_freq
+		idf[word] = math.log(total_tweets/doc_word_freq)
+	#print len(idf) #4881
+
+	tfidf = []
+	for item in arr_tf:
+		temp_tfidf = {}
+		for k, v in item.items():
+			idf_val = idf[k]
+			temp_tfidf = idf[k]*v
+		tfidf.append(temp_tfidf)
+
+	for i in tfidf:
+		print i
 
 get_tweets()
